902207ef733e269144323dc1166ec967f8eac9df
h6wk <130506947+h6wk@users.noreply.github.com>
2023-04-26 21:28:08 +0000

    Save document into MongoDB

--- index.js
+++ index.js
@@ -12,13 +12,39 @@ app.listen(3000, () => {
 });
 
 
-mongoose.set("strictQuery", false);
-
 // Define the database URL to connect to.
-const mongoDB = "mongodb+srv://testuser:pWtO6ZPNfgNE8vyw@cluster0.xrgraod.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://testuser:pWtO6ZPNfgNE8vyw@cluster0.xrgraod.mongodb.net/helloworld?retryWrites=true&w=majority";
 
 // Wait for database to connect, logging an error if there is a problem
-main().catch((err) => console.log(err)).then(() => console.log("Database connected"));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser:true
+  , useUnifiedTopology:true})
+  .then(() => console.log("Connected"))
+  .catch((err) => console.error(err));
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    reEnterPassword: String,
+    dob: Number,
+    phone: Number,
+    address: String,
+    pincode: Number
+});
+
+const User = new mongoose.model("User", userSchema);
+
+const user = new User(
+  {firstName:'fname', lastName:'lName', email:'email'
+   , password:'pwd', reEnterPassword:'pwd', dob:111222
+   , phone:120312, address:'adr', pincode:1234}
+);
+
+//user.validate();
+
+user.save().then(savedDoc => {
+  savedDoc === user; // true
+});
